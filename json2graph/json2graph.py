0482109118ffd310491575816138a12cfd03f32e
Philip Fritzsche <p-f@users.noreply.github.com>
2020-02-04 13:51:48 +0100

    Add proper CLI and option to filter type

--- json2graph/json2graph.py
+++ json2graph/json2graph.py
@@ -1,7 +1,8 @@
+import argparse
 import json
 
 
-def json2graph(inpath, outpath):
+def json2graph(inpath, outpath, remove_atoms=[]):
     file = open(inpath, "r")
     outfile = open(outpath, "w")
     parsed = json.load(file)
@@ -17,9 +18,30 @@ def json2graph(inpath, outpath):
     outfile.write("Edges labelled;True\n")
     outfile.write("Directed graph;False\n")
     outfile.write("\n")
+    removed_atom_ids = []
     for atom in range(nr_of_atoms):
-        outfile.write(str(compound["aid"][atom]) + ";" + str(compound["element"][atom]) + "\n")
+        atom_type = str(compound["element"][atom])
+        atom_id = str(compound["aid"][atom])
+        if atom_type in remove_atoms:
+            removed_atom_ids += [atom_id]
+            continue
+        outfile.write(atom_id + ";" + atom_type + "\n")
     outfile.write("\n")
     for bond in range(nr_of_bonds):
-        outfile.write(str(bonds["aid1"][bond]) + ";" + str(bonds["aid2"][bond]) + ";" + str(bonds["order"][bond]) + "\n")
+        source_id = str(bonds["aid1"][bond])
+        target_id = str(bonds["aid2"][bond])
+        if source_id in removed_atom_ids or target_id in removed_atom_ids:
+            continue
+        outfile.write(source_id + ";" + target_id + ";" +
+                      str(bonds["order"][bond]) + "\n")
     outfile.close()
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--filter-atom", help="Filtered atom types (comma "
+                                             "reparated)")
+    ap.add_argument("-i", "--input", help="Input file", required=True)
+    ap.add_argument("-o", "--output", help="Output file", required=True)
+    args = ap.parse_args()
+    json2graph(args.input, args.output, args.filter_atom.split(","))
--- json2graph/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from json2graph import json2graph
-
-if __name__ == '__main__':
-    if (len(sys.argv) < 3):
-        print("Usage: JSONIN GRAPHOUTOUT")
-    else:
-        json2graph(sys.argv[1], sys.argv[2])
