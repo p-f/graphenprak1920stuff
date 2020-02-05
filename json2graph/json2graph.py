d9b800fca45b948545f0414fbf4a90786095fabd
Philip Fritzsche <p-f@users.noreply.github.com>
2020-02-05 10:45:03 +0100

    Fix node/edge count when filter is on.
    Now stores all nodes and edges in lists and writes the whole file at
    once.

--- json2graph/json2graph.py
+++ json2graph/json2graph.py
@@ -4,36 +4,41 @@ import json
 
 def json2graph(inpath, outpath, remove_atoms=[]):
     file = open(inpath, "r")
-    outfile = open(outpath, "w")
     parsed = json.load(file)
     file.close()
     compound = parsed["PC_Compounds"][0]["atoms"]
     bonds = parsed["PC_Compounds"][0]["bonds"]
-    outfile.write("AUTHOR: Egal.\n")
     nr_of_atoms = len(compound["aid"])
-    outfile.write("#nodes;" + str(nr_of_atoms) + "\n")
     nr_of_bonds = len(bonds["aid1"])
-    outfile.write("#edges;" + str(nr_of_bonds) + "\n")
-    outfile.write("Nodes labelled;True\n")
-    outfile.write("Edges labelled;True\n")
-    outfile.write("Directed graph;False\n")
-    outfile.write("\n")
     removed_atom_ids = []
+    vertices = []
+    edges = []
     for atom in range(nr_of_atoms):
         atom_type = str(compound["element"][atom])
         atom_id = str(compound["aid"][atom])
         if atom_type in remove_atoms:
             removed_atom_ids += [atom_id]
             continue
-        outfile.write(atom_id + ";" + atom_type + "\n")
-    outfile.write("\n")
+        vertices += [(atom_id, atom_type)]
     for bond in range(nr_of_bonds):
         source_id = str(bonds["aid1"][bond])
         target_id = str(bonds["aid2"][bond])
         if source_id in removed_atom_ids or target_id in removed_atom_ids:
             continue
-        outfile.write(source_id + ";" + target_id + ";" +
-                      str(bonds["order"][bond]) + "\n")
+        edges += [(source_id, target_id, str(bonds["order"][bond]))]
+    outfile = open(outpath, "w")
+    outfile.write("AUTHOR: Egal.\n")
+    outfile.write("#nodes;" + str(len(vertices)) + "\n")
+    outfile.write("#edges;" + str(len(edges)) + "\n")
+    outfile.write("Nodes labelled;True\n")
+    outfile.write("Edges labelled;True\n")
+    outfile.write("Directed graph;False\n")
+    outfile.write("\n")
+    for vertex in vertices:
+        outfile.write(";".join(vertices) + "\n")
+    outfile.write("\n")
+    for edge in edges:
+        outfile.write(";".join(edges) + "\n")
     outfile.close()
 
 
