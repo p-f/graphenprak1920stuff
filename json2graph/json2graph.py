d52241c6a23b6455828ef9f0a741e83ba7f9b8cd
Philip Fritzsche <p-f@users.noreply.github.com>
2020-02-07 15:59:48 +0100

    Add option to read .graph format

--- json2graph/json2graph.py
+++ json2graph/json2graph.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 
+from enum import Enum
+
 
 class Graph:
     """
@@ -115,40 +117,125 @@ class Graph:
             outfile.write(";".join(edge) + "\n")
         outfile.close()
 
+    @classmethod
+    def read_graph(cls, in_path: str, in_format="auto"):
+        """
+        Read a graph in either json or .graph format.
 
-def json2graph(inpath, outpath, remove_atoms=[], author=None):
-    graph = read_graph(inpath)
+        :param in_path: The input path.
+        :param in_format: The input format (or auto).
+        :return: A new graph read from the file
+        """
+        if (in_format == "auto" and in_path.endswith("json"))\
+                or in_format == "json":
+            return Graph.read_graph_json(in_path)
+        elif (in_format == "auto" and in_path.endswith("graph")) \
+                or in_format == "graph":
+            return Graph.read_graph_graph(in_path)
+        else:
+            raise Exception("Unknown format " + in_format + " or format not "
+                                                            "detected.")
+
+    @classmethod
+    def read_graph_graph(cls, in_path):
+        """
+        Read a graph from a .graph file.
+
+        :param in_path: The input path.
+        :return: The parsed graph.
+        """
+        infile = open(in_path, "r")
+        vertices = []
+        edges = []
+        author = None
+        vertex_count = None
+        edge_count = None
+        line: str = infile.readline()
+        state = 1
+        while line:
+            line = line.rstrip()
+            if state == 1:
+                # Header
+                if line.startswith("//"):
+                    pass
+                elif line.startswith("AUTHOR:"):
+                    author = line[7:]
+                elif line.startswith("#nodes;"):
+                    vertex_count = line.split(";")[1]
+                elif line.startswith("#edges;"):
+                    edge_count = line.split(";")[1]
+                elif line.startswith("Nodes labelled;"):
+                    if not bool(line.split(";")[1]):
+                        raise Exception("Unlabelled nodes are not supported")
+                elif line.startswith("Edges labelled;"):
+                    if not bool(line.split()):
+                        raise Exception("Unlabelled edges are not supported")
+                elif line.startswith("Directed graph"):
+                    pass
+                elif line == "":
+                    state = 2
+                else:
+                    raise Exception("Unrecognized line: " + line)
+            if state == 2:
+                # Validify header
+                assert vertex_count, "Vertex count not set"
+                assert edge_count, "Edge count not set"
+                state = 3
+            elif state == 3:
+                # Vertices
+                if line == "":
+                    state = 4
+                else:
+                    data = line.split(";")
+                    if len(data) != 2:
+                        raise Exception("Failed to parse vertex: " + line)
+                    vertices += [(data[0], data[1])]
+            elif state == 4:
+                # Edges
+                data = line.split(";")
+                if len(data) != 3:
+                    raise Exception("Failed to parse edge: " + line)
+                else:
+                    edges += [(data[0], data[1], data[2])]
+            line = infile.readline()
+        infile.close()
+        return Graph(vertices, edges)
+
+    @classmethod
+    def read_graph_json(cls, inpath):
+        """
+        Read a graph from a file in JSON format. This lists of vertices and edges
+        as tuples. Vertices are encoded as tuples of (id, label) and edges as
+        tuples of (source_id, target_id, label).
+
+        :param inpath: The JSON file input path.
+        :return: A graph as tuple of vertex- and edge-list.
+        """
+        file = open(inpath, "r")
+        parsed = json.load(file)
+        file.close()
+        compound = parsed["PC_Compounds"][0]["atoms"]
+        bonds = parsed["PC_Compounds"][0]["bonds"]
+        nr_of_atoms = len(compound["aid"])
+        nr_of_bonds = len(bonds["aid1"])
+        vertices = []
+        edges = []
+        for atom in range(nr_of_atoms):
+            atom_type = str(compound["element"][atom])
+            atom_id = str(compound["aid"][atom])
+            vertices += [(atom_id, atom_type)]
+        for bond in range(nr_of_bonds):
+            source_id = str(bonds["aid1"][bond])
+            target_id = str(bonds["aid2"][bond])
+            edges += [(source_id, target_id, str(bonds["order"][bond]))]
+        return Graph(vertices, edges)
+
+
+def json2graph(inpath: str, outpath: str, informat: str,
+               remove_atoms=[], author=None):
+    graph = Graph.read_graph(inpath, informat)
     graph.filter_vertices(lambda x: x[1] not in remove_atoms or (x[1] in remove_atoms and graph.have_neighbors_multibonds(x))) \
         .write(outpath, author)
-
-
-def read_graph(inpath):
-    """
-    Read a graph from a file in JSON format. This lists of vertices and edges
-    as tuples. Vertices are encoded as tuples of (id, label) and edges as
-    tuples of (source_id, target_id, label).
-
-    :param inpath: The JSON file input path.
-    :return: A graph as tuple of vertex- and edge-list.
-    """
-    file = open(inpath, "r")
-    parsed = json.load(file)
-    file.close()
-    compound = parsed["PC_Compounds"][0]["atoms"]
-    bonds = parsed["PC_Compounds"][0]["bonds"]
-    nr_of_atoms = len(compound["aid"])
-    nr_of_bonds = len(bonds["aid1"])
-    vertices = []
-    edges = []
-    for atom in range(nr_of_atoms):
-        atom_type = str(compound["element"][atom])
-        atom_id = str(compound["aid"][atom])
-        vertices += [(atom_id, atom_type)]
-    for bond in range(nr_of_bonds):
-        source_id = str(bonds["aid1"][bond])
-        target_id = str(bonds["aid2"][bond])
-        edges += [(source_id, target_id, str(bonds["order"][bond]))]
-    return Graph(vertices, edges)
 
 
 if __name__ == '__main__':
@@ -157,6 +244,8 @@ if __name__ == '__main__':
                                                 "separated)", default="")
     ap.add_argument("-i", "--input", help="Input file", required=True)
     ap.add_argument("-o", "--output", help="Output file", required=True)
+    ap.add_argument("-l", "--format", help="Input format (json, graph, auto)",
+                    default="auto")
     ap.add_argument("-a", "--author", help="Author", default="Egal.",
                     required=False)
     ap.add_argument("-A", "--no-author", help="Do not write the author.",
@@ -165,4 +254,5 @@ if __name__ == '__main__':
     new_author = args.author
     if args.no_author:
         new_author = None
-    json2graph(args.input, args.output, args.filter_atom.split(","), new_author)
+    filter_atoms = args.filter_atom.split(",")
+    json2graph(args.input, args.output, args.format, filter_atoms, new_author)
